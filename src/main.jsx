590e3e95d7bd3cc019412868dd742a035c034846
Anand-Robosoft <anand_karuppusamy@discovery.com>
2023-12-02 10:48:05 +0530

    UserList path change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import UserList from "./pages/UserList.jsx";
+import UserList from "./pages/userList/UserList.jsx";
 import { Userprovider } from "./store/UserContext";
 import "./index.css";
 
